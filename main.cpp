fddbc40cab5da82591a12bfe4f1999da49271501
P0m1dorka <loginovandrej31@gmail.com>
2024-11-26 19:26:18 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
+    // <b>lang</b> variable name to see how CLion can help you rename it.
+    std::cout << "Hello World!\n";
+    return 0;
+}
